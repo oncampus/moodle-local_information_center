ec0feac8707ed88ec179d2e438a5ee0bc87b0d70
Konrad Ebel <konrad.ebel@oncampus.de>
2026-04-17 15:05:35 +0200

    Allow lower moodle versions to use the pipeline

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2026041700;
-$plugin->requires  = 2025041400;
+$plugin->requires  = 2024100100;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->component = 'local_information_center';
 $plugin->release = '2.0.0';
